92aefb884b93c76b4a0552ec45945ec1d256a5eb
25001502 <25001502@mvula.univen.ac.za>
2026-03-23 12:50:56 +0200

    pointers

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
-void showValues(int values[], int size);
+
 
 int main() {
 
-int myValues[] = {1, 2, 3, 4, 5};
-int size = sizeof(myValues) / sizeof(myValues[0]);
-showValues(myValues, size);
+int a= 100;
+int *intprt = &a;
+
+std::cout << *intprt << std::endl;
 
 }
 
-void showValues(int values[], int size) {
-    std::cout << "Values: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << values[i] << " ";
-    }
-    std::cout << std::endl;
-}
