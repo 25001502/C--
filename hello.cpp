ca891d042b3d8c7ab7bcf485c9598a6bf0d80ef5
25001502 <25001502@mvula.univen.ac.za>
2026-03-23 12:11:52 +0200

    funtions learned

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 
+void calculateSum(int a, int b, int c);
+
 int main() {
 
-    int a = 5;
-    int b = 10;
-    int add = a + b;
+    int a;
+    int b;
+    int c;
+    
 
-     std:: cout << add;
+    a = 5;
+    b = 10;
+    c = 15;
 
+    calculateSum(a,b,c);
+    
+
+    return 0;
+
+}
+
+void calculateSum(int a, int b, int c) {
+    int sum = a + b + c;
+    std::cout << sum;
 }
